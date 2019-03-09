0c40d9c3ef0259b8de04c6913d18f739d5bba53a
若山侑輝 <wakayamayuki@wakayamayuuakira-no-Mac-mini.local>
2019-03-09 22:22:51 +0900

    スキャフォードでマイグレーションを作成したら登録処理等も自動 でできるようになった？　一覧画面のhtml修正まで

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -4,7 +4,7 @@ class LinesController < ApplicationController
   # GET /lines
   # GET /lines.json
   def index
-    @lines = Line.all
+    @lines = Line.all.order(id: "DESC")
   end
 
   # GET /lines/1
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,3 @@
 class Line < ApplicationRecord
+  validates :content, presence: true,length: { maximum: 140 }
 end
