028a1583ce69d03b81fc562f204146900f834313
若山侑輝 <wkym_4a@icloud.com>
2019-03-15 00:38:51 +0900

    更新モードのエラー時、ボタン名称が変わってしまう不具合を修正

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -3,16 +3,17 @@ module LinesHelper
   #  binding.pry
     if action_name == 'new' || action_name == 'confirm'
       confirm_lines_path
-    elsif action_name == 'edit'
+    elsif action_name == 'edit' || action_name == 'update'
       line_path
     end
   end
 
 
   def choose_new_or_edit_BtnName
+    binding.pry
     if action_name == 'new' || action_name == 'confirm'
         "新規登録"
-    elsif action_name == 'edit'
+    elsif action_name == 'edit' || action_name == 'update'
         "更新"
     end
   end
@@ -20,7 +21,7 @@ module LinesHelper
     def choose_new_or_edit_Method
       if action_name == 'new' || action_name == 'confirm'
           "GET"
-      elsif action_name == 'edit'
+      elsif action_name == 'edit' || action_name == 'update'
           "PUT"
       end
     end
